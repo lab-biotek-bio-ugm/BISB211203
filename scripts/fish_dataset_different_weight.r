aab8b0982e43eae66a8a8f4af9a4185ddcb83c42
Matin Nuhamunada <matinnuhamunada@gmail.com>
2025-05-15 05:43:28 +0700

    feat: add t test example

--- scripts/fish_dataset_different_weight.r
+++ scripts/fish_dataset_different_weight.r
@@ -1,12 +1,14 @@
 set.seed(123)
 
-# Adjusted: smaller difference in means
+# Same length distribution
 length_upstream <- rnorm(50, mean = 28, sd = 3)
-weight_upstream <- rnorm(50, mean = 330, sd = 35)
+length_downstream <- rnorm(50, mean = 28, sd = 3)
 
-length_downstream <- rnorm(50, mean = 26, sd = 3)
-weight_downstream <- rnorm(50, mean = 310, sd = 35)
+# Significantly different weight
+weight_upstream <- rnorm(50, mean = 340, sd = 30)
+weight_downstream <- rnorm(50, mean = 300, sd = 30)
 
+# Combine data
 lake <- c(rep("Upstream", 50), rep("Downstream", 50))
 length <- c(length_upstream, length_downstream)
 weight <- c(weight_upstream, weight_downstream)
@@ -18,4 +20,4 @@ fish_data <- data.frame(
 )
 
 # Export to CSV
-write.csv(fish_data, "assets/data/fish_data.csv", row.names = FALSE)
+write.csv(fish_data, "assets/data/fish_data_weight.csv", row.names = FALSE)
